5fa2000e96a518193db800866a07e19de43afc61
Joey Eisenzimmer <joey.eisenzimmer@gmail.com>
2018-08-13 14:46:11 -0700

    updated html and js file with locations and data

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict'
+
+var hoursOpen = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+
+var firstAndPike = {
+    location: '1st and Pike',
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookiesPerSale: 6.3,
+
+};
+
+var seaTac = {
+    location: 'SeaTac Airport',
+    minCustomer: 3,
+    maxCustomer: 24,
+    avgCookiesPerSale: 1.2,
+
+};
+
+var seattleCenter = {
+    location: 'Seattle Center',
+    minCustomer: 11,
+    maxCustomer: 38
+    avgCookiesPerSale: 3.7,
+
+};
+
+var capitolHill = {
+    location: 'Capitol Hill',
+    minCustomer: 20,
+    maxCustomer: 38,
+    avgCookiesPerSale: 2.3,
+};
+
+var alki = {
+    location: 'Alki',
+    minCustomer: 2,
+    maxCustomer: 16,
+    avgCookiesPerSale: 4.6,
+
+};
