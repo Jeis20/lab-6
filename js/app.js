222dc64b5b393d8c3378d1ccb3a78b893b7aa3ca
Joey Eisenzimmer <joey.eisenzimmer@gmail.com>
2018-08-16 21:08:01 -0700

    table and forms mostly complete

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ var hoursOpen = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm',
 var storeLocations = [];
 var footerTotals = [];
 
+var cookieForm = document.getElementById('cookie-form');
+var updateForm = document.getElementById('update-form');
+
 var patTable = document.getElementById('patTable');
 
 function Bakery(name, minHourlyCustomer, maxHourlyCustomer, avgCookiesPerSale) {
@@ -48,12 +51,12 @@ seattleCenter.calcDailyCookieTotal();
 capitolHill.calcDailyCookieTotal();
 alki.calcDailyCookieTotal();
 
-// console.table(storeLocations);
-// console.log(pike);
-// console.log(seaTac);
-// console.log(seattleCenter);
-// console.log(capitolHill);
-// console.log(alki);
+console.table(storeLocations);
+console.log(pike);
+console.log(seaTac);
+console.log(seattleCenter);
+console.log(capitolHill);
+console.log(alki);
 
 Bakery.prototype.render = function() {
   var trEl = document.createElement('tr');
@@ -105,7 +108,7 @@ function createFooter() {
   thEl.textContent = 'Hourly Totals';
   trEl.appendChild(thEl);
   patTable.appendChild(trEl);
-
+  console.log(createFooter);
 
   for (var i = 0; i < hoursOpen.length; i++) {
     var tdEl = document.createElement('td');
@@ -131,42 +134,44 @@ for(var i = 0; i < hoursOpen.length; i++) {
   footerTotals[i] = 0;
   for(var j = 0; j < storeLocations.length; j++) {
     footerTotals[i] += storeLocations[j].cookiesPerHour[i];
+    console.log(footerTotals);
   }
 
 }
+function newBakerySubmit (event) {
+  event.preventDefault();
 
-// function renderAll() {
-//   for (var i = 0; i < storeLocations; i++)
+  if (!event.target.bakery.value || !event.target.min.value || !event.target.max.value || !event.target.avg.value) {
+    return alert ('All fields required, please resubmit! Danke!');
+  }
 
-//     storeLocations[i].render(i);
-// }
+  var newName = event.target.bakery.value;
+  var newMin = event.target.min.value;
+  var newMax = event.target.max.value;
+  var newAvg = event.target.avg.value;
+  var newBakery = new Bakery (newName, newMin, newMax, newAvg);
 
+  console.table(storeLocations);
+
+  newBakery.render();
+
+  event.target.store.value = null;
+  event.target.min.value = null;
+  event.target.max.value = null;
+  event.target.avg.value = null;
+}
+
+cookieForm.addEventListener('submit', newBakerySubmit);
 makeHeaderRow();
 displaySales();
 createFooter();
-// renderAll();
 
-// submit button event listener
-// addForm.addEventListener('submit', handleFormSubmit);
+updateForm.addEventListener('click', function() {
+  // Empties input fields
+  cookieForm.innerHTML = '';
 
-// function handleFormSubmit(event) {
-// event.preventDefault();
-
-// var newLocation = event.target.newLocation.value;
-// var minimumCustomer = parseInt(event.target.minimumCustomer.value);
-// var maximumCustomer = parseInt(event.target.maximumCustomer.value);
-// var avgCookiesPerCustomer = parseInt(event.target.avgCookiesPerCustomer.value);
-
-// }
-
-patTable.innerHTML = '';
-makeHeaderRow();
-displaySales();
-createFooter();
-// renderAll();
-
-// clear form values
-event.target.newLocation.value = null;
-event.target.minimumCustomer.value = null;
-event.target.maximumCustomer.value = null;
-event.target.avgCookiesPerCustomer.value = null;
+  patTable.innerHTML = '';
+  makeHeaderRow();
+  displaySales();
+  createFooter();
+});
